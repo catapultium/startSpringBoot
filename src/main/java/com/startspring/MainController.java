1547fcadb10dc8b4b1fdc9de570e6dd1067bdb18
deplax <deplax2@gmail.com>
2016-11-24 22:31:19 +0900

    프론트 소스 추가

---
+++ src/main/java/com/startspring/MainController.java
@@ -0,0 +1,28 @@
+package com.startspring;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+/**
+ * Created by Deplax on 2016. 11. 24..
+ */
+
+@Controller
+public class MainController {
+
+    @RequestMapping("/")
+    public String index(){
+        return "index";
+    }
+
+    @RequestMapping("/login")
+    public String login(){
+        return "login";
+    }
+
+    @RequestMapping("/signUp")
+    public String signUp(){
+        return "signUp";
+    }
+
+}
