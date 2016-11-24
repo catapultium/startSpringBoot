f5202c4afc38ca9a7dce38022419f183dd46fca7
whale <whale.so@kakaocorp.com>
2016-11-24 14:00:57 +0900

    타임리프3.0 적용

--- src/main/java/com/startspring/StartSpringBootApplication.java
+++ src/main/java/com/startspring/StartSpringBootApplication.java
@@ -5,46 +5,47 @@ import org.slf4j.LoggerFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.thymeleaf.ThymeleafAutoConfiguration;
 import org.springframework.context.annotation.Bean;
 
 
 // @SpringBootApplication same as @Configuration @EnableAutoConfiguration @ComponentScan
-@SpringBootApplication
+@SpringBootApplication(exclude = ThymeleafAutoConfiguration.class)
 public class StartSpringBootApplication {
 
-	private static final Logger log = LoggerFactory.getLogger(StartSpringBootApplication.class);
+    private static final Logger log = LoggerFactory.getLogger(StartSpringBootApplication.class);
 
-	public static void main(String[] args) {
-		SpringApplication.run(StartSpringBootApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(StartSpringBootApplication.class, args);
+    }
 
-	@Bean
-	public CommandLineRunner demo(CustomerRepository repository){
-		return (args) -> {
-			repository.save(new Customer("Jack", "Bauer"));
-			repository.save(new Customer("Chloe", "O'Brian"));
-			repository.save(new Customer("Kim", "Bauer"));
-			repository.save(new Customer("David", "Palmer"));
-			repository.save(new Customer("Michelle", "Dessler"));
+    @Bean
+    public CommandLineRunner demo(CustomerRepository repository) {
+        return (args) -> {
+            repository.save(new Customer("Jack", "Bauer"));
+            repository.save(new Customer("Chloe", "O'Brian"));
+            repository.save(new Customer("Kim", "Bauer"));
+            repository.save(new Customer("David", "Palmer"));
+            repository.save(new Customer("Michelle", "Dessler"));
 
-			log.info("Customers found with findAll():");
-			log.info("-------------------------------");
-			for (Customer customer : repository.findAll()){
-				log.info(customer.toString());
-			}
-			log.info("");
+            log.info("Customers found with findAll():");
+            log.info("-------------------------------");
+            for (Customer customer : repository.findAll()) {
+                log.info(customer.toString());
+            }
+            log.info("");
 
-			Customer customer = repository.findOne(1L);
-			log.info("Customer found with findOne(1L):");
-			log.info("--------------------------------");
-			log.info(customer.toString());
-			log.info("");
+            Customer customer = repository.findOne(1L);
+            log.info("Customer found with findOne(1L):");
+            log.info("--------------------------------");
+            log.info(customer.toString());
+            log.info("");
 
-			log.info("Customer found with findByLastName('Bauer'):");
-			log.info("--------------------------------------------");
-			for(Customer bauer : repository.findByLastName("Bauer")){
-				log.info(bauer.toString());
-			}
-		};
-	}
+            log.info("Customer found with findByLastName('Bauer'):");
+            log.info("--------------------------------------------");
+            for (Customer bauer : repository.findByLastName("Bauer")) {
+                log.info(bauer.toString());
+            }
+        };
+    }
 }
